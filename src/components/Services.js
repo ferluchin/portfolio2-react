deac455cc237f950fb89283243bf35ac527434fc
Luis Granda <lgrandab@gmail.com>
2023-07-19 20:20:27 -0500

    updated services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,40 +1,52 @@
 import React from "react";
+import "./Services.css";
 
 function Services() {
     return (
         <section className="my-services" id="services">
             <h2 className="section__title section__title--services">
-                What I do
+                My Expertise
             </h2>
             <div className="services">
                 <div className="service">
-                    <h3>Design</h3>
+                    <h3>Design & User Experience</h3>
                     <p>
-                        I strive to create beautiful and intuitive web apps,
-                        meaning I design with both aestetics and usability in
-                        mind. You will get your designs delivered as Figma
-                        files, as websites or both.
+                        With a passion for aesthetics and functionality, I
+                        design intuitive and attractive web applications. My
+                        designs are delivered as Figma files, websites, or both,
+                        always prioritizing usability.
                     </p>
                 </div>
                 {/* <!-- / service --> */}
 
                 <div className="service">
-                    <h3>Development</h3>
+                    <h3>Web & Mobile Development</h3>
                     <p>
-                        I am a versatile frontend developer with a speciality in
-                        React JS and Tailwind CSS. I also have a solid grip of
-                        the fundamentals of web development using HTML, CSS, and
-                        JavaScript.
+                        As a versatile frontend developer with a specialization
+                        in React JS and Angular Framework, I offer effective
+                        development solutions. I have a solid grasp of the
+                        fundamentals of web development including HTML, CSS, and
+                        JavaScript, and experience in mobile app development
+                        with Swift.
                     </p>
                 </div>
                 {/* <!-- / service --> */}
 
-                {/* <!-- Add more services if you want --> */}
+                <div className="service">
+                    <h3>Data Analysis & Machine Learning</h3>
+                    <p>
+                        With an analytical focus, I optimize large scale data
+                        processing and apply data modeling techniques and trend
+                        analysis. I also have experience in developing and
+                        implementing machine learning algorithms.
+                    </p>
+                </div>
+                {/* <!-- / service --> */}
             </div>
             {/* <!-- / services --> */}
 
             <a href="#work" className="btn">
-                My Projects
+                See My Projects
             </a>
         </section>
     );
