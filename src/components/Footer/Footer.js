b8f32e31bef7efb9c41dc29a7a9f2b845aad4f23
ferluchin <lgrandab@gmail.com>
2024-12-26 21:55:20 -0500

    update pagina

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,44 +1,57 @@
 //Filename: Footer.js
 //comando emmet rfce
-
 import React from "react";
 import "./Footer.css";
 
 function Footer() {
     return (
-        <footer className="footer">
+    <footer className="footer">
             {/* <!-- replace with your own email address --> */}
-            <a href="mailto:lgrandab@gmail.com" className="footer__link">
-                lgrandab@gmail.com
+        <a href="mailto:lgrandab@gmail.com" className="footer__link">
+        lgrandab@gmail.com
+        </a>
+        <ul className="social-list">
+            <li className="social-list__item">
+                <a
+                    className="social-list__link"
+                    href="https://codepen.io/ferluchin"
+                    target="_blank"
+                    rel="noopener noreferrer"
+            >
+                <i className="fab fa-codepen"></i>
             </a>
-            <ul className="social-list">
-                <li className="social-list__item">
-                    <a
-                        className="social-list__link"
-                        href="https://codepen.io/ferluchin"
-                    >
-                        <i className="fab fa-codepen"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a className="social-list__link" href="http://dribbble.com">
-                        <i className="fab fa-dribbble"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a className="social-list__link" href="https://twitter.com">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                </li>
-                <li className="social-list__item">
-                    <a
-                        className="social-list__link"
-                        href="https://github.com/ferluchin"
-                    >
-                        <i className="fab fa-github"></i>
-                    </a>
-                </li>
-            </ul>
+        </li>
+        <li className="social-list__item">
+            <a
+                className="social-list__link"
+                href="http://dribbble.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-dribbble"></i>
+            </a>
+        </li>
+        <li className="social-list__item">
+            <a
+                className="social-list__link"
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-twitter"></i>
+            </a>
+            </li>
+            <li className="social-list__item">
+            <a
+                className="social-list__link"
+                href="https://github.com/ferluchin"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-github"></i>
+            </a>
+            </li>
+        </ul>
         </footer>
     );
 }
